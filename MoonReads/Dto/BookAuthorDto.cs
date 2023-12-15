0151320758b42d7c376b63174f571b880bda51f5
meef3k <mateuszjawor99@gmail.com>
2023-12-15 16:20:13 +0100

    Remove id from relations dto

--- MoonReads/Dto/BookAuthorDto.cs
+++ MoonReads/Dto/BookAuthorDto.cs
@@ -2,7 +2,6 @@ namespace MoonReads.Dto
 {
     public class BookAuthorDto
     {
-        public int Id { get; set; }
         public int BookId { get; set; }
         public int AuthorId { get; set; }
     }
--- MoonReads/Dto/BookCategoryDto.cs
+++ MoonReads/Dto/BookCategoryDto.cs
@@ -2,7 +2,6 @@ namespace MoonReads.Dto
 {
     public class BookCategoryDto
     {
-        public int Id { get; set; }
         public int BookId { get; set; }
         public int CategoryId { get; set; }
     }
